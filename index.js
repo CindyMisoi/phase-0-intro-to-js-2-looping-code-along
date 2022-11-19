662b2803427f720cfc6a6f47d3d3a01e48896268
CindyMisoi <crmisoi2k@gmail.com>
2022-11-19 23:09:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solutions in this file
+/*for(let age = 30; age < 40; age++){
+    console.log(`I am ${age} years old. Happy birthday to me!`);
+    //debugger;
+}*/
+
+const gifts = ["teddy bear","drone","doll"];
+
+function wrapGifts(gifts){
+ for(let i = 0; i < gifts.length; i++){
+  console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  //debugger;
+ };
+ return gifts;
+};
+wrapGifts(gifts);
+
+function writeCards(names, eventName){
+ let messages = [];
+ for(let j = 0; j < names.length; j++){
+ messages.push(`Thank you, ${names[j]}, for the wonderful ${eventName} gift!`);
+ debugger;
+ };
+ return messages;
+};
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+function countDown(){
+    let i = 10;
+    while(i >= 0){
+     console.log(i);
+     i--;
+    };
+};
+countDown(10);
